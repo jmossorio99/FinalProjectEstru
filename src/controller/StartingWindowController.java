cde998fd4326d430afb196b4e2c2b891ff5a43f4
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-05-11 13:55:28 -0500

    added window features and updated its controller

--- src/controller/StartingWindowController.java
+++ src/controller/StartingWindowController.java
@@ -1,5 +1,52 @@
 package controller;
 
-public class StartingWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class StartingWindowController implements Initializable{
+
+	ObservableList<String> OL = FXCollections.observableArrayList("Time","Money","Distance");
+	
+	@FXML
+    private TextField OriginButton;
+
+    @FXML
+    private TextField DestinyButton;
+
+    @FXML
+    private Button SearchButton;
+
+    @FXML
+    private ChoiceBox<String> FilterChoiceBox;
+
+    @FXML
+    private TextArea RouteTextArea;
+
+    @FXML
+    private Label QuantityTextField;
+
+    @FXML
+    void searchClicked(ActionEvent event) {
+
+    }
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	
+		
+		FilterChoiceBox.setItems(OL);
+		
+		
+	}
 
 }
