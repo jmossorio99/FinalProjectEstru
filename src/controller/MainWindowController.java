69143d49c3ba5f13f177dfdf6791b23356eefcb5
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-05-17 12:30:52 -0500

    created and connected the controller

---
+++ src/controller/MainWindowController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+
+public class MainWindowController implements Initializable{
+
+	@FXML
+    private TextField fromTextField;
+
+    @FXML
+    private TextField toTextField;
+
+    @FXML
+    private Label cityLabel;
+
+    @FXML
+    void allCityButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goButtonPressed(ActionEvent event) {
+
+    }
+
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/view/Main.java
+++ src/view/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 	public void start(Stage window) throws Exception {
 
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/view/StartingWindowView.fxml"));
+		loader.setLocation(getClass().getResource("/view/MainWindow.fxml"));
 		Scene scene = new Scene(loader.load());
 		window.setScene(scene);
 		window.show();
